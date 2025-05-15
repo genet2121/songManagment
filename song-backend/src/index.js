d1c978f8f8601b59ef0d14d25ca4c62999f64e7a
genet2121 <tesfayerusalem@gmail.com>
2025-05-15 11:25:47 +0300

    issue fix

--- song-backend/src/index.js
+++ song-backend/src/index.js
@@ -23,12 +23,12 @@ if (!process.env.MONGO_URI) {
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
-
+const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch(err => console.error(err));
